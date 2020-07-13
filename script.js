bc89786362ab1396dcb53c61280e97e13c96b1e5
mightyprinces <happyrabbit62@gmail.com>
2020-07-13 10:07:15 +0300

    added block teams in html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// MENU BURGER
+
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeBtn = document.querySelector('.close');
@@ -15,4 +17,25 @@ menu.addEventListener('click', e => {
   if(!e.target.classList.contains('menu__link')) {
     document.body.classList.remove('body--with_opened_menu');
   }
+})
+
+
+// REVIEWS SLIDER
+
+const findBlockByAlias = alias => {
+  return $('.reviews__item').filter((ndx, item) => {
+    return $(item).attr('data-linked-with') === alias;
+  })
+}
+
+$('.interactive-avatar__link').click(e => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr('data-open');
+  const itemToShow = findBlockByAlias(target);
+  const curItem = $this.closest('.interactive-avatar');
+
+  itemToShow.addClass('active').siblings().removeClass('active');
+  curItem.addClass('interactive-avatar--active').siblings().removeClass('interactive-avatar--active');
 })
