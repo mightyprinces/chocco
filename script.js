29e4ac5ecc31b32f92fb3bbded9b5cba95943d05
mightyprinces <happyrabbit62@gmail.com>
2020-07-17 10:26:52 +0300

    finished section 'team' and 'slider'

--- script.js
+++ script.js
@@ -39,3 +39,55 @@ $('.interactive-avatar__link').click(e => {
   itemToShow.addClass('active').siblings().removeClass('active');
   curItem.addClass('interactive-avatar--active').siblings().removeClass('interactive-avatar--active');
 })
+
+// ACCORDION
+
+const openItem = item => {
+  const container = item.closest('.team__item');
+  const contentBlock = container.find('.team__content');
+  const textBlock = contentBlock.find('.team__content-block');
+  const reqHeight = textBlock.height();
+
+  container.addClass('active');
+  contentBlock.height(reqHeight);
+}
+
+const closeEveryItem = container => {
+  const items = container.find('.team__content');
+  const itemContainer = container.find('.team__item');
+
+  itemContainer.removeClass('active');
+  items.height(0);
+}
+
+$('.team__title').click(e => {
+  const $this = $(e.currentTarget);
+  const container = $this.closest('.team');
+  const elemContainer = $this.closest('.team__item');
+
+  if (elemContainer.hasClass('active')) {
+    closeEveryItem(container);
+  } else {
+    closeEveryItem(container);
+    openItem($this);
+  };
+});
+
+// SLIDER
+
+$(document).ready(function(){
+  const slider = $('.slider__list').bxSlider({
+    pager: false,
+    controls: false
+  });
+
+  $('.slider__arrow--prev').click(e => {
+    e.preventDefault();
+    slider.goToPrevSlide();
+  });
+
+  $('.slider__arrow--next').click(e => {
+    e.preventDefault();
+    slider.goToNextSlide();
+  });
+});
